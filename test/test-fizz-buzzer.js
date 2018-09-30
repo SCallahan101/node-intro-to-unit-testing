caef6d90a6757776bae3c073290b356fea1f4672
Shannon Callahan <ShannonC619@gmail.com>
2018-09-30 11:55:04 -0600

    first commit attempt

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,31 @@
+const chai = require('chai').expect;
+
+const fizzBuzzer = require('/fizzBuzzer');
+
+describe('fizzBuzzer', function() {
+
+  it('the input can be any number as long it is one input', function() {
+    const goodInputs = [
+      [1],
+      [10],
+      [100]
+    ];
+    goodInputs(function(input) {
+      const answer = fizzBuzzer(input);
+      expect(answer).to.equal(input.expected);
+    });
+  });
+  
+  it('The input is non-numeric so therefore it is invalid input', function() {
+    const badInputs = [
+      ['hello'],
+      ['1'],
+      [....]
+    ];
+    badInputs(function(input) {
+    expect(function() {
+      fizzBuzzer([0]);
+    }).to.throw(Error);
+  });
+  });
+});
